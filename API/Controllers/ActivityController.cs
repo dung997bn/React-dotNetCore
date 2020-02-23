3fa88124a392b84256c0bd77823c8e4b5986282f
DUNG <dung@gmail.com>
2020-02-23 22:21:50 +0700

    End of Section 4

---
+++ API/Controllers/ActivityController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ActivityController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> List()
+        {
+            return await _mediator.Send(new List.Query());
+        }
+
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Activity>> Details(Guid Id)
+        {
+            return await _mediator.Send(new Details.Query { Id = Id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create([FromBody] Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid Id, [FromBody] Edit.Command command)
+        {
+            command.Id = Id;
+            return await _mediator.Send(command);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid Id)
+        {
+            return await _mediator.Send(new Delete.Command { Id = Id });
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +34,7 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddJsonOptions(opt =>
             {
                 opt.SerializerSettings.ContractResolver = new DefaultContractResolver();
